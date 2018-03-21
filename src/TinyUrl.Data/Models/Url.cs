5115e1fe72f8517ad110c968933ec5824d4b6339
Redouane Sobaihi <o-red@outlook.com>
2018-03-21 19:21:52 +0100

    Add comments.

--- src/TinyUrl.Data/Models/Url.cs
+++ src/TinyUrl.Data/Models/Url.cs
@@ -4,11 +4,31 @@ using System.Text;
 
 namespace TinyUrl.Data.Models
 {
+    /// <summary>
+    /// Represents a URL entity.
+    /// </summary>
     public class Url
     {
         public int Id { get; set; }
+
+        /// <summary>
+        /// Visits counter.
+        /// </summary>
         public long VisitCount { get; set; }
+
+        /// <summary>
+        /// Original URL.
+        /// </summary>
         public string OriginalUrl { get; set; }
+
+        /// <summary>
+        /// Base64 string for URL hash.
+        /// </summary>
+        public string UrlHash { get; set; }
+
+        /// <summary>
+        /// Short code for URL. (Without domain)
+        /// </summary>
         public string ShortUrl { get; set; }
     }
 }
