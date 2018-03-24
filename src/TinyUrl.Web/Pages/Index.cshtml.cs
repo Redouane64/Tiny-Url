c18395f2a4461f0bd25cd5489fd0b8d3a2dfc27f
Redouane Sobaihi <o-red@outlook.com>
2018-03-24 20:59:56 +0100

    WIP: URL shortening web interface.

---
+++ src/TinyUrl.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TinyUrl.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPostAsync()
+        {
+            // TODO : Catch URL here.
+
+            await Task.CompletedTask;
+        }
+    }
+}
